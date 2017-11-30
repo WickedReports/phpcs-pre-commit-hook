9792cbc0c7345f543482c09034c164de5ac65d52
stanma <malkin.stanislav@gmail.com>
2017-11-30 16:52:39 +0200

    autoload

---
+++ src/Installer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Installer
+{
+    public static function postInstall()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            system('cmd /c vendor/wickedreports/phpcs-pre-commit-hook/src/setup.bat');
+        }
+        else {
+            system('sh vendor/wickedreports/phpcs-pre-commit-hook/src/setup.sh');
+        }
+    }
+}
--- src/install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    system('cmd /c vendor/wickedreports/phpcs-pre-commit-hook/src/setup.bat');
-}
-else {
-    system('sh vendor/wickedreports/phpcs-pre-commit-hook/src/setup.sh');
-}
