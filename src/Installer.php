cd5b346ace99b07374f402e307d4bde559ebcf09
stanma <malkin.stanislav@gmail.com>
2017-11-30 18:55:14 +0200

    bugfix

--- src/Installer.php
+++ src/Installer.php
@@ -7,7 +7,7 @@ class Installer
     public static function postInstall()
     {
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            system('cmd /c vendor/wickedreports/phpcs-pre-commit-hook/src/setup.bat');
+            system('cmd /c vendor\wickedreports\phpcs-pre-commit-hook\src\setup.bat');
         }
         else {
             system('sh vendor/wickedreports/phpcs-pre-commit-hook/src/setup.sh');
